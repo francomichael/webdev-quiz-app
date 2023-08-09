76125a4369ddaf274a5b1298a8af8bda6beafe07
francomichael <megagrip@icloud.com>
2023-08-09 23:08:15 +0200

    Changes to HTML
    Comments deleted.

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+    ]
+	},
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+    ]
+	},
+    {
+    numb: 3,
+    question: "What is the correct HTML element for creating a line break?",
+    answer: "br",
+    options: [
+    "br",
+    "linebreak",
+    "htmlbreak",
+    "b"
+    ]
+	},
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+    ]
+	},
+    {
+    numb: 5,
+    question: "How do you declare a variable in JavaScript?",
+    answer: "var, let, or const",
+    options: [
+    "veri",
+    "ver",
+    "var, let, or const",
+    "vary, init, que"
+    ]
+	},
+    {
+    numb: 6,
+    question: "Which language uses /* */ as a comment?",
+    answer: "CSS",
+    options: [
+    "JavaScript",
+    "PHP",
+    "HTML",
+    "CSS"
+    ]
+    },
+    {
+    numb: 7,
+    question: "What is the DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+    "Document Object Model",
+    "Dos Object Modem",
+    "Delete On Memo ",
+    "Degree Of Momentum"
+    ]
+    },
+    {
+    numb: 8,
+    question: "Which HTML tag is used to create a hyperlink?",
+    answer: "a",
+    options: [
+    "hl",
+    "hyper",
+    "HyLink",
+    "a"
+    ]
+    },
+    {
+    numb: 9,
+    question: "How do you convert a string to an integer?",
+    answer: "parseInt()",
+    options: [
+    "s2i",
+    "parseInt()",
+    "interPass",
+    "strInt"
+    ]
+    },
+    {
+    numb: 10,
+    question: "What is an example of an event in JavaScript?",
+    answer: "Clicking a button or pressing a key.",
+    options: [
+    "Opening your browser.",
+    "is to view errors within your console.log",
+    "Events don't exist.",
+    "Clicking a button or pressing a key."
+    ]
+    },
+];
